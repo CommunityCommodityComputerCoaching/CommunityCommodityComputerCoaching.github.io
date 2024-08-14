0f8a01720ea150af33751291046bd4490e2012fc
Josh McCall <joshmccall@microsoft.com>
2024-08-14 12:46:58 -0500

    astro actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: 'https://CommunityCommodityComputerCoaching.github.io',
+  base: 'c4.giving',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
