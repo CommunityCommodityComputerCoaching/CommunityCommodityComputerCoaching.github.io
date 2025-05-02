296471f78414cf05327f2f44ffdefc65a016ba8c
Sean Newcomb <43507629+sknewcomb@users.noreply.github.com>
2025-05-01 22:59:37 -0500

    Adding docker to allow for local development

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,20 @@ export default defineConfig({
   site: 'https://CommunityCommodityComputerCoaching.github.io',
   base: '/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  
+  server: {
+    host: true
+  },
+
+  vite: {
+    server: {
+      host: true,
+      hmr: {
+        host: 'localhost',
+        clientPort: 4321 // or another port that's available
+      },
+      network: "host",
+      allowedHosts: true
+    },
+  },
 });
